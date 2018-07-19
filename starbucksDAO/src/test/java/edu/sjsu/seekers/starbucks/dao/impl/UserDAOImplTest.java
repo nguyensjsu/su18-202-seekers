18effa1858dc3adff9d7580a1ab53c4ffa41dca7
Nidhijmr <nidhi.jmr@gmail.com>
2018-07-19 02:03:02 -0700

    change in app config, removing old user dao

--- starbucksDAO/src/test/java/edu/sjsu/seekers/starbucks/dao/impl/UserDAOImplTest.java
+++ starbucksDAO/src/test/java/edu/sjsu/seekers/starbucks/dao/impl/UserDAOImplTest.java
@@ -1,54 +1,55 @@
 package edu.sjsu.seekers.starbucks.dao.impl;
 
 import edu.sjsu.seekers.starbucks.StarbucksApplication;
-import edu.sjsu.seekers.starbucks.UnitTestDatabaseConfig;
-import edu.sjsu.seekers.starbucks.dao.repository.UserRepository;
-import edu.sjsu.seekers.starbucks.model.User;
-import org.junit.Before;
-import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.junit4.SpringRunner;
-
-import javax.sql.DataSource;
-import java.sql.SQLException;
-import java.util.Optional;
-
-import static org.junit.Assert.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest(classes = {StarbucksApplication.class,UnitTestDatabaseConfig.class})
+@SpringBootTest(classes = {StarbucksApplication.class})
 @ActiveProfiles("unit-test")
 public class UserDAOImplTest {
 
-
-    @Autowired
-    DataSource dataSource;
-
-    @Autowired
-    UserRepository userRepository;
-
-
-
-    @Test
-    public void get() throws SQLException {
-        System.out.println("Our DataSource is = " + dataSource.getConnection());
-//        User user = new User();
-//        user.setFullname("test test");
-//        user.setUsername("test");
-//        userRepository.save(user);
-
-        Iterable<User> users;
-        users = userRepository.findAll();
-        for(User usr : users)
-            System.out.println("test user is: " + usr);
-
-    }
+//    private EmbeddedDatabase db;
+//
+//
+//    @Autowired
+//    UserRepository userRepository;
+//
+//    @Before
+//    public void setUp() {
+//
+////        ClassLoader classLoader = getClass().getClassLoader();
+////        File file = new File(classLoader.getResource("db/sql/create-db.sql").getFile());
+////        System.out.println(file.getAbsolutePath());
+//
+//        File resources1 = new File("src/test/resources/db/sql/dbCreate.sql");
+//        System.out.println(resources1.getAbsolutePath());
+//
+//        File resources2 = new File("src/test/resources/db/sql/insert.sql");
+//        System.out.println(resources2.getAbsolutePath());
+//
+//        db = new EmbeddedDatabaseBuilder()
+//                .setType(EmbeddedDatabaseType.H2)
+//                .addScript(resources1.getAbsolutePath())
+//                .addScript(resources2.getAbsolutePath())
+//                .build();
+//    }
+//
+//
+//    @Test
+//    public void get() throws SQLException {
+//
+////        User user = new User();
+////        user.setFullname("test test");
+////        user.setUsername("test");
+////        userRepository.save(user);
+//
+//        Iterable<User> users;
+//        users = userRepository.findAll();
+//        for(User usr : users)
+//            System.out.println("test user is: " + usr);
+//
+//    }
 }
