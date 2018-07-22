7ecccaa2428a6c5c137ac8fc70176c73018cab93
SindhuGSpatil <sssindhupatil@gmail.com>
2018-07-21 23:28:03 -0700

    User and Address DAO methods implemented

--- starbucksDAO/src/main/java/edu/sjsu/seekers/starbucks/dao/repository/UserRepository.java
+++ starbucksDAO/src/main/java/edu/sjsu/seekers/starbucks/dao/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package edu.sjsu.seekers.starbucks.dao.repository;
 
 import edu.sjsu.seekers.starbucks.model.User;
+import org.hibernate.annotations.DynamicUpdate;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -20,9 +21,17 @@ public interface UserRepository extends JpaRepository<User, Integer> {
     @Query("SELECT u FROM User u WHERE u.emailId = :emailId")
     Optional<User> findByEmailId(@Param("emailId") String emailId);
 
+    @Query("SELECT u.userKey FROM User u WHERE u.userName = :userName")
+    Integer getUserKey(@Param("userName") String userName);
+
+    @Query("SELECT u.password FROM User u WHERE u.userName = :userName")
+    String getPassword(@Param("userName") String userName);
+
     @Transactional
     @Modifying
-    @Query("UPDATE User u SET u.password = :password WHERE u.userName = :userName")
-    int resetPassword(@Param("password") String password, @Param("userName") String userName);
+    @Query(value = "UPDATE User u SET u.password = :password WHERE u.userKey = :userKey")
+    int resetPassword(@Param("userKey") Integer userKey, @Param("password") String password);
+
+
 
 }
