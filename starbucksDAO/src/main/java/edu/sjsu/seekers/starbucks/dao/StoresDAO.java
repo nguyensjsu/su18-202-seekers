d789f83cc1f2a8d7249d762306e5a6c22896b465
Nidhijmr <nidhi.jmr@gmail.com>
2018-07-21 22:48:48 -0700

    get all stores DAO added

--- starbucksDAO/src/main/java/edu/sjsu/seekers/starbucks/dao/StoresDAO.java
+++ starbucksDAO/src/main/java/edu/sjsu/seekers/starbucks/dao/StoresDAO.java
@@ -1,13 +1,16 @@
 package edu.sjsu.seekers.starbucks.dao;
 
 import edu.sjsu.seekers.starbucks.model.ProductCatalog;
+import edu.sjsu.seekers.starbucks.model.Products;
 import edu.sjsu.seekers.starbucks.model.Stores;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface StoresDAO {
 
     /*public Optional<Stores> findStoresByAddressKey(Integer addressKey);*/
     public Optional<Stores> get(Integer id);
+    public List<Stores> getAll();
     /*public Optional<Stores> findStoresNearUser(Integer addressKey);*/
 }
