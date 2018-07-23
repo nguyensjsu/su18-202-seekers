b22d8b8df37a53aa6adff39976c8f902213835f7
Ashwinisnv <ashwini.snv@gmail.com>
2018-07-23 00:00:08 -0700

    Payment API changes

--- PaymentAPI/src/main/java/edu/sjsu/seekers/PaymentAPI/service/impl/PaymentOptionsServiceImpl.java
+++ PaymentAPI/src/main/java/edu/sjsu/seekers/PaymentAPI/service/impl/PaymentOptionsServiceImpl.java
@@ -7,10 +7,13 @@ import edu.sjsu.seekers.PaymentAPI.service.PaymentOptionsService;
 import edu.sjsu.seekers.starbucks.dao.PaymentCardDetailsDAO;
 import edu.sjsu.seekers.starbucks.dao.PaymentDetailsDAO;
 import edu.sjsu.seekers.starbucks.dao.UserDAO;
+import edu.sjsu.seekers.starbucks.dao.impl.UserDAOImpl;
+import edu.sjsu.seekers.starbucks.model.PaymentCardDetails;
 import edu.sjsu.seekers.starbucks.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -31,8 +34,9 @@ public class PaymentOptionsServiceImpl implements PaymentOptionsService {
     @Override
     public PaymentOptionsResponse getPaymentOptions(String userName) {
         Optional<User> user = userDAO.findUserByUsername(userName);
-        System.out.println(user);
-//        paymentCardDetailsDAO.findPaymentCardDetailsByUserKey(user.);
+        System.out.println(user.get().getUserKey());
+        List<PaymentCardDetails> paymentCards = paymentCardDetailsDAO.findPaymentCardDetailsByUserKey(user.get().getUserKey());
+        System.out.println(paymentCards);
         return null;
     }
 }
