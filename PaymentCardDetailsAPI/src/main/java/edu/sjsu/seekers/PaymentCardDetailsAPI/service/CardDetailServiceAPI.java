5c2aa5c4ebca170e37e8a071caab8a9f21cfc1c8
AnuradhaIyer <tulsiraj14@gmail.com>
2018-07-25 00:33:33 -0700

    API for saving, updating and deleting card details

--- PaymentCardDetailsAPI/src/main/java/edu/sjsu/seekers/PaymentCardDetailsAPI/service/CardDetailServiceAPI.java
+++ PaymentCardDetailsAPI/src/main/java/edu/sjsu/seekers/PaymentCardDetailsAPI/service/CardDetailServiceAPI.java
@@ -1,6 +1,8 @@
 package edu.sjsu.seekers.PaymentCardDetailsAPI.service;
 
 import edu.sjsu.seekers.PaymentCardDetailsAPI.Request.CardDetailsRequest;
+import edu.sjsu.seekers.PaymentCardDetailsAPI.Request.DeleteCardRequest;
+import edu.sjsu.seekers.PaymentCardDetailsAPI.Request.UpdateExistingCardRequest;
 import edu.sjsu.seekers.PaymentCardDetailsAPI.Response.CardDetailsResponse;
 import edu.sjsu.seekers.PaymentCardDetailsAPI.Response.GenericResponse;
 import edu.sjsu.seekers.PaymentCardDetailsAPI.Response.SaveCardsResponse;
@@ -14,4 +16,8 @@ public interface CardDetailServiceAPI {
     CardDetailsResponse getCardDetails(Integer userId);
 
     SaveCardsResponse saveCardDetails(CardDetailsRequest request);
+
+    GenericResponse UpdateCard(UpdateExistingCardRequest request);
+
+    GenericResponse deleteCard(DeleteCardRequest request);
 }
